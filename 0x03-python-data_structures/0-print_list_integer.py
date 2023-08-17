c61b20e5a496dfb8e3416c25cc87d4062e6e1635
bazileros <zalisilesithule@gmail.com>
2023-08-17 02:30:32 +0200

    Readme && matrix simple

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 0-print_list_integer.py
 
-def print_list_integers(my_list=[]):
+def print_list_integer(my_list=[]):
     '''Print all integers of the list'''
     for num in my_list:
         print("{:d}".format(num))
