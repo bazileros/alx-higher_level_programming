c50b2ac6849c32a01b6a3845c646ea42270ff7dc
bazileros <zalisilesithule@gmail.com>
2023-11-18 00:06:57 +0200

    filter by n

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    lists all states with a name starting with N
+    from the database hbtn_0e_0_usa
+    Usage: ./1-filter_states.py <username> <password> <database>
+"""
+
+import MySQLdb
+import sys
+
+def fetch_states_with_initial_n(username, password, database):
+    try:
+        with MySQLdb.connect(user=username, passwd=password, db=database) as db:
+            with db.cursor() as cursor:
+                cursor.execute("SELECT * FROM `states` ORDER BY `id`")
+                states = [state for state in cursor.fetchall() if state[1][0] == 'N']
+                return states
+    except MySQLdb.Error as e:
+        print(f"❌ Error: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("⚠️ Usage: python3 1-filter_states.py <username> <password> <database>")
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    
+    result = fetch_states_with_initial_n(username, password, database)
+    
+    for state in result:
+        print(state)
