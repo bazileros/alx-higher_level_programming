e182ad7a560cbabe0107bb02a19918a9e2bca9c5
bazileros <zalisilesithule@gmail.com>
2023-11-19 11:52:05 +0200

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,16 +10,14 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
-
     try:
         with sql.connect(user=username, passwd=password, db=database) as db:
             with db.cursor() as cursor:
                 cursor.execute(
-                    "SELECT * FROM `states` WHERE name = %s ORDER BY id ASC",
-                    (state_name,),
+                    "SELECT * FROM `states` WHERE  BINARY `name` = '{}' ORDER BY id ASC"
+                    .format(state_name)
                 )
-                states = cursor.fetchall()
-                for state in states:
-                    print(state)
+                [print(state) for state in cursor.fetchall()]
+
     except sql.Error as e:
         print(f"❌ Error: {e}")
