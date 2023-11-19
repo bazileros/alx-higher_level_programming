e1b605e9fafc89711f0f65210e4eccd20449be68
bazileros <zalisilesithule@gmail.com>
2023-11-19 12:03:20 +0200

    sql injection proof

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    takes in an argument and displays all values in the states table of
+    hbtn_0e_0_usa where name matches the argument
+"""
+
+import sys
+import MySQLdb as sql
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    try:
+        with sql.connect(user=username, passwd=password, db=database) as db:
+            with db.cursor() as cursor:
+                cursor.execute(
+                    "SELECT * FROM `states` WHERE  BINARY `name` = %s\
+                    ORDER BY id ASC", (state_name,)
+                )
+                [print(state) for state in cursor.fetchall()]
+
+    except sql.Error as e:
+        print(f"❌ Error: {e}")
