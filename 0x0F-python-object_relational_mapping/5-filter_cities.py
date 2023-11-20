7ac75c42167690301df457238cd052a2e4d8a0ca
bazileros <zalisilesithule@gmail.com>
2023-11-20 02:20:23 +0200

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-""" takes in the name of a state as an argument and
-    lists all cities of that state, using the database hbtn_0e_4_usa
+"""Displays all cities of a given state from the
+states table of the database hbtn_0e_4_usa.
+Safe from SQL injections.
+Usage: ./5-filter_cities.py <mysql username> \
+                            <mysql password> \
+                            <database name> \
+                            <state name searched>
 """
 
-import MySQLdb as sql
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    try:
-        with sql.connect(user=username, passwd=password, db=database) as db:
-            with db.cursor() as cursor:
-                cursor.execute(
-                    "SELECT cities.id, cities.name\
-                    FROM cities\
-                    INNER JOIN states\
-                    ON cities.state_id = states.id\
-                    WHERE states.name = %s\
-                    ORDER BY cities.id ASC",
-                    (state_name,),
-                )
-                [print(city) for city in cursor.fetchall()]
-
-    except sql.Error as e:
-        print(f"❌ Error: {e}")
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
